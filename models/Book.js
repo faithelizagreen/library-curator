2569dc896d43822d7d929ada322247d8cde7d251
tehshaw <25413002+tehshaw@users.noreply.github.com>
2021-11-01 23:34:26 -0400

    Add book model and seed

---
+++ models/Book.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isbn: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_paperback: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+    subject: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    pages: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    // reader_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'reader',
+    //     key: 'id',
+    //   },
+    // },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'books'
+  }
+);
+
+module.exports = Book;
