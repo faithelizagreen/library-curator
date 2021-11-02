8cd52735feca837d4113da5858676d1f5b4e47af
Travis Puryear <travispuryear678@gmail.com>
2021-11-02 12:01:48 -0400

    Update Reader.js

--- models/Reader.js
+++ models/Reader.js
@@ -16,10 +16,15 @@ Reader.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    firstname: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+      
     email: {
       type: DataTypes.STRING,
       allowNull: false,
