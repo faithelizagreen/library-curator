6eb9cb8286d8beb02667292c755057bc4949c3fb
tehshaw <25413002+tehshaw@users.noreply.github.com>
2021-11-02 10:21:48 -0400

    Add models for reader and lbcard

--- models/index.js
+++ models/index.js
@@ -1,25 +1,28 @@
-// const Reader = require('./Reader');
+const Reader = require('./Reader');
 const Book = require('./Book');
-// const LibraryCard = require('./LibraryCard');
+const LibraryCard = require('./LibraryCard');
 
-// Reader.hasOne(LibraryCard, {
-//   foreignKey: 'reader_id',
-//   onDelete: 'CASCADE',
-// });
+Reader.hasOne(LibraryCard, {
+    foreignKey: 'reader_id',
+    onDelete: 'CASCADE',
+});
 
-// Reader.hasMany(Book, {
-//   foreignKey: 'reader_id',
-//   onDelete: 'CASCADE',
-// });
+LibraryCard.belongsTo(Reader, {
+    foreignKey: 'reader_id',
+});
 
-// Book.belongsTo(Reader, {
-//   foreignKey: 'reader_id',
-// });
 
-// LibraryCard.belongsTo(Reader, {
-//   foreignKey: 'reader_id',
-// });
 
-module.exports = { Book };
-// module.exports = { Reader, Book, LibraryCard };
+Reader.hasMany(Book, {
+    foreignKey: 'reader_id',
+    onDelete: 'CASCADE',
+});
+
+Book.belongsTo(Reader, {
+    foreignKey: 'reader_id',
+});
+
+
+
+module.exports = { Reader, Book, LibraryCard };
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,22 +1,24 @@
 const sequelize = require('../config/connection');
-const { Book } = require('../models');
+const { Book, Reader, LibraryCard } = require('../models');
 
-// const readerSeedData = require('./readerSeedData.json');
+const readerSeedData = require('./readerSeedData.json');
 const bookSeedData = require('./bookSeedData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+    await sequelize.sync({ force: true });
 
-//   const readers = await Reader.bulkCreate(readerSeedData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
+    const readers = await Reader.bulkCreate(readerSeedData, {
+        individualHooks: true,
+        returning: true,
+    });
 
-//   for (const { id } of readers) {
-//     const newCard = await LibraryCard.create({
-//       reader_id: id,
-//     });
-//   }
+    for (const { id, is_admin } of readers) {
+        if(!is_admin){
+        const newCard = await LibraryCard.create({
+        reader_id: id,
+        });
+        }
+    }
 
 //   for (const book of bookSeedData) {
 //     const newBook = await Book.create({
@@ -24,21 +26,20 @@ const seedDatabase = async () => {
 //       reader_id: readers[Math.floor(Math.random() * readers.length)].id,
 //     });
 //   }
-    let count = 1;
 
     try{
-    const books = await Book.bulkCreate(bookSeedData, {
-            individualHooks: true,
-            returning: false,
-            logging: false,
-          });
-        }
-        catch{
-            console.log();
-        }
-        finally{
-            console.log("Done");
-        }
+        const books = await Book.bulkCreate(bookSeedData, {
+                individualHooks: true,
+                returning: false,
+                logging: false,
+            });
+    }
+    catch{
+        console.log();
+    }
+    finally{
+        console.log("Done");
+    }
 
   process.exit(0);
 };
