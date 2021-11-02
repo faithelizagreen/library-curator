f0d14096f1f4eee260793cfdcf63a98d1205c077
faithelizagreen <faithgreen@knights.ucf.edu>
2021-11-02 11:17:01 -0500

    added public folder

---
+++ public/js/books.js
@@ -0,0 +1,27 @@
+const addBookLibraryDB = async (event) => {
+    
+    event.preventDefault();
+
+    const googleKey = AIzaSyArrMjqUSxtIDaXs97e6AAynyVPSaJO0ao;
+    const googleBooks = "https://www.googleapis.com/books/v1/volumes?q=react&key=" + googleKey
+
+    if(googleBooks) {
+
+    const response = await fetch( googleBooks, {
+        method: "GET",
+        headers: {"Content-Type" : "application/json"},
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to search book');
+      }
+    }
+};
+
+document
+  .querySelector('.add-book-form')
+  .addEventListener('search', addBookLibraryDB);
+
+
