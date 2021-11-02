631b141ec120ba16da4dd75576b3303de7691c10
Travis Puryear <travispuryear678@gmail.com>
2021-11-02 15:19:18 -0400

    Update login.js
    Changed to readers from users

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (userID && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/readers/login', {
         method: 'POST',
         body: JSON.stringify({ userID, password }),
         headers: { 'Content-Type': 'application/json' },
