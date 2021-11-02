7839d002392bca93c16c93b7e2c341b6c4ca28e7
Travis Puryear <travispuryear678@gmail.com>
2021-11-02 14:40:02 -0400

    Library card API call routes.

--- controllers/api/libraryCard.js
+++ controllers/api/libraryCard.js
@@ -1,11 +1,31 @@
-const router= require('express').Router()
+const { LibraryCard, Reader } = require('../../models');
 
+const router = require('express').Router();
 
+router.get('/', (req, res) => {
+  LibraryCard.findAll({
+    attributes: ['card_number', 'id'],
+    include: {
+      model: Reader,
+      attributes: ['id', 'first_name', 'last_name', 'email'],
+    },
+  })
+    .then((CardData) => res.json(CardData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
+router.get('/:id', (req, res) => {
+  LibraryCard.findOne({
+    where: {
+      id: req.params.id,
+    },include:{model:Reader, attributes:['id','first_name','last_name','email']}
+  })
+    .then((cardData) => res.json(cardData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-
-
-
-
-
-module.exports = router 
+module.exports = router;
