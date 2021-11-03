26c67f9a55b447c78d65db3c9de0b2cb1bce8e6c
Travis Puryear <travispuryear678@gmail.com>
2021-11-03 15:40:32 -0400

    login layout.

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 const router = require("express").Router()
 const apiRoutes = require("./api")
 const homeRoute = require ("./homeRoute")
-
+const profileRoutes = require("./readerProfile")
 
 
 router.use("/", homeRoute)
 router.use("/api", apiRoutes);
+router.use("/profile", profileRoutes);
 
 
 module.exports = router
